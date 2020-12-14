80494f409381f3a6b6979d2bd0ba09e5c4ec60ef
Pavel Kirilin <win10@list.ru>
2020-12-14 17:35:13 +0400

    Fixed main readabillity.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 try:
-    from aopi.runners.gunicorn_runner import run_app as gunicorn_app
+    from aopi.runners.gunicorn_runner import run_app
 except ImportError:
-    gunicorn_app = None  # type: ignore
-
-try:
-    from aopi.runners.asyncio_runner import run_app as aiohttp_app
-except ImportError:
-    aiohttp_app = None  # type: ignore
+    from aopi.runners.asyncio_runner import run_app
 
 
 def main() -> None:
-    if gunicorn_app:
-        gunicorn_app()
-    elif aiohttp_app:
-        aiohttp_app()
-    else:
-        print(
-            "Can't run the application. "
-            "Try to install aopi with unix extra"
-            " (python -m pip install aopi[unix])."
-        )
+    run_app()
 
 
 if __name__ == "__main__":
